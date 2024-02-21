9aaae7a20a4b10140bcf5af8c2a24b90b4f0f10b
chenlianghong <clh021@gmail.com>
2024-02-21 22:14:55 +0800

    cmd: update

--- cmd/v1/gen/cmd.go
+++ cmd/v1/gen/cmd.go
@@ -86,20 +86,25 @@ func (c cGEN) Index(ctx context.Context, in cInput) (out *cOutput, err error) {
 	table := cfg.MustGet(ctx,"gfcli.gen.dao.table").String()
 	log.Printf("link: %s", link)
 	log.Printf("table: %s", table)
-	db.TestGetComment()
-	// getTableStruct(link, table, ctx)
+	// db.TestGetComment()
+	getTableStruct(link, table, ctx)
 
-	// 进行时，发现 gf 是会处理表注释的，只是 sqlite 没有像 mysql 一样直接支持。
-	// 按照本项目的思路
-	// 修改 sqlite 对应的代码文件
-	// contrib/drivers/sqlite/sqlite_table_fields.go
-	// contrib/drivers/sqlite/sqlite_z_unit_core_test.go
-	// 像 mysql 对应的代码文件一样即可
-	// contrib/drivers/mysql/mysql_table_fields.go
-	// contrib/drivers/mysql/mysql_z_unit_core_test.go
-	// 尝试直接修改这个部分，也许还能合并进去，及时不能合并进去，也可以先自己用。
-	// 就这么干……
 	// 再下一步就可以写 crud 了。
+	// 本命令只做一件事情，就是 crud 。
+	// 项目维护时，要尽可能的能够顺利调用所有可利用资源。
+	// 比如:
+	// - 可以直接调用 dao 。如果不能调用外部包，就直接拷贝进来，最小化同步包文件的维护。
+	// - 要先可以直接生成 api
+	// - 其次可以直接生成定制化的 ctrl
+	// - 其次可以直接生成 logic
+	// - 其次可以直接调用生成 service
+	// - 然后要可以直接生成前端代码
+	// - api 响应的结构体
+	// - api 请求的结构体
+	// - api 请求的方法
+	// - 创建数据的表单页面
+	// - 数据列表页面，支持搜索翻页，前端翻页
+	// - 数据列表页面，支持搜索翻页，后端翻页
 	// gcmd.CommandFromCtx(ctx).Print()
 	return
 }
@@ -115,6 +120,16 @@ func getTableStruct(link, table string, ctx context.Context) {
 	}
 	log.Println("MergeTables:")
 	g.Dump(tables)
+	// 进行时，发现 gf 是会处理表注释的，只是 sqlite 没有像 mysql 一样直接支持。
+	// 按照本项目的思路
+	// 修改 sqlite 对应的代码文件
+	// contrib/drivers/sqlite/sqlite_table_fields.go
+	// contrib/drivers/sqlite/sqlite_z_unit_core_test.go
+	// 像 mysql 对应的代码文件一样即可
+	// contrib/drivers/mysql/mysql_table_fields.go
+	// contrib/drivers/mysql/mysql_z_unit_core_test.go
+	// 尝试直接修改这个部分，也许还能合并进去，及时不能合并进去，也可以先自己用。
+	// 就这么干……
 	fields, err := Db.Fields(tables[0])
 	if err != nil {
 		mlog.Fatal(err)
