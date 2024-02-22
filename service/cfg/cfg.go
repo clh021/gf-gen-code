4981d428f9f9c4c4fa9f51aac135cb57a7a8e232
chenlianghong <clh021@gmail.com>
2024-02-22 14:59:12 +0800

    update cfg comment

--- service/cfg/cfg.go
+++ service/cfg/cfg.go
@@ -9,11 +9,22 @@ import (
 
 func GetByFilePath(ctx context.Context, path string) (cfg *gcfg.Config, err error) {
 	// 默认配置文件行为
-	// 会按照文件后缀toml/yaml/yml/json/ini/xml/properties自动检索配置文件。
-	// 当前工作目录,当前可执行文件所在目录,当前main源代码包所在目录
-	// ./
-	// ./config
-	// ./manifest/config
+	// 单例对象在创建时会按照文件后缀toml/yaml/yml/json/ini/xml/properties自动检索配置文件。
+	// 默认情况下会自动检索配置文件config.toml/yaml/yml/json/ini/xml/properties并缓存，配置文件在外部被修改时将会自动刷新缓存。
+	//
+	// 当前工作目录
+	// USER_PWD/
+	// USER_PWD/config
+	// USER_PWD/manifest/config
+	// 当前可执行文件所在目录
+	// BIN_PATH/
+	// BIN_PATH/config
+	// BIN_PATH/manifest/config
+	// 当前main源代码包所在目录
+	// CODE_PATH/
+	// CODE_PATH/config
+	// CODE_PATH/manifest/config
+	// GF_DEBUG=true ./main // 有时也许可以帮上忙
 	// ------------------------------------------------------------------
 	// 这里将消除默认行为，直接读取用户配置的配置文件路径
 	// cfgDir := gfile.Dir(cfgPath)
