9c3658b827132a949215eee115f95b3898905309
chenlianghong <clh021@gmail.com>
2024-03-07 11:49:59 +0800

    vendor: cmd/gf/genapi run ok

--- vendor/github.com/gogf/gf/cmd/gf/v2/internal/cmd/genapi/gen_templates/api.go.tpl
+++ vendor/github.com/gogf/gf/cmd/gf/v2/internal/cmd/genapi/gen_templates/api.go.tpl
@@ -7,71 +7,52 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
-type {{ .TableName }}SearchReq struct {
-	g.Meta  `path:"/{{ .TableName }}/search" method:"get" summary:"查询用户列表(搜索分页)" tags:"用户管理"`
+// 搜索
+type {{ .data.TableName | ToCamel }}SearchReq struct {
+	g.Meta  `path:"/{{ .data.TableName | ToSnake }}/search" method:"get" summary:"查询用户列表(搜索分页)" tags:"用户管理"`
 	Search  string `json:"search"   v:"length:0,99#搜索用户名|名长度为:{min}到:{max}位" dc:"用户编号"`
 	OrderBy string `json:"order_by" v:"length:0,30#排序字段不正确"       d:"id"        dc:"排序字段"`
 	Order   string `json:"order"    v:"in:desc,asc#排序方式不正确"       d:"order"     dc:"排序方式"`
 	model.CommonPaginationReq
 }
-type {{ .tableName }}SearchRes struct {
-	List    []*entity.{{ .tablename }} `json:"list"                          dc:"用户列表"` //
-	OrderBy string         `json:"order_by"        d:"id"        dc:"排序字段不正确"`
-	Desc    string         `json:"desc"            d:"desc"      dc:"排序方式不正确"`
+type {{ .data.TableName | ToCamel }}SearchRes struct {
+	List    []*entity.{{ .data.TableName | ToCamel }} `json:"list"                                              dc:"用户列表"` //
+	OrderBy string         `json:"order_by"                            d:"id"        dc:"排序字段不正确"`
+	Desc    string         `json:"desc"                                d:"desc"      dc:"排序方式不正确"`
 	model.CommonPaginationRes
 }
-type UserCreateReq struct {
-	g.Meta     `path:"/user/create" method:"post" tags:"用户管理" summary:"新建用户"`
-	Name       string `json:"name"        v:"required|min-length:4"                dc:"用户名"`  //
-	Email      string `json:"email"                                                dc:"邮箱"`   //
-	Address    string `json:"address"                                              dc:"地址"`   //
-	Mark       string `json:"mark"                                                 dc:"备注"`   //
-	Permission string `json:"permission"                                           dc:"权限设置"` //
-	Password   string `json:"password"    v:"required|ci|same:RePassword"          dc:"用户密码"` //
-	RePassword string `json:"repassword"  v:"required"                             dc:"重复密码"` //
+
+// 创建
+type {{ .data.TableName | ToCamel }}CreateReq struct {
+	g.Meta     `path:"/{{ .data.TableName | ToSnake }}/create" method:"post" tags:"用户管理" summary:"新建用户"`{{range $cname, $c := .data.Fields }}{{if eq $c.Name "created_at" "updated_at" "deleted_at"}}{{else}}
+	{{$c.Name | ToCamel }} {{$c.GType}} `json:{{$c.JsonTag | ToPadAndQuote}}                               dc:"{{$c.Comment}}"`{{end}}{{end}}
 }
-type UserCreateRes struct {
+type {{ .data.TableName | ToCamel }}CreateRes struct {
 	NewID uint `json:"new_id"   dc:"ID"`
 }
-type UserResetPwdReq struct {
-	g.Meta `path:"/user/reset_pwd/{id}" method:"post" tags:"用户管理" summary:"重置用户密码"`
-	Id     uint `json:"id" in:"path"  v:"min:1#用户的编号不正确"                  dc:"用户编号"`
-	// Password   string `json:"password"      v:"required|password|ci|same:RePassword" dc:"用户密码"` //
-	Password   string `json:"password"      v:"required|same:RePassword" dc:"用户密码"`             //
-	RePassword string `json:"repassword"    v:"required"                             dc:"重复密码"` //
+
+// 查一个
+type {{ .data.TableName | ToCamel }}OneReq struct {
+	g.Meta `path:"/{{ .data.TableName | ToSnake }}/one/{id}" method:"get" tags:"用户管理" summary:"获取一个"`
+	Id     uint `json:"id" in:"path"  v:"min:1#编号不正确"                  dc:"编号"`
 }
-type UserResetPwdRes struct {
+type {{ .data.TableName | ToCamel }}OneRes struct {
+	*entity.{{ .data.TableName | ToCamel }}
 }
-type UserDeleteReq struct {
-	g.Meta `path:"/user/delete/{id}" method:"post" tags:"用户管理" summary:"删除指定用户"`
+
+// 修改
+type {{ .data.TableName | ToCamel }}UpdateReq struct {
+	g.Meta `path:"/{{ .data.TableName | ToSnake }}/update/{id}" method:"post" tags:"用户管理" summary:"重置密码"`
+	Id     uint `json:"id" in:"path"  v:"min:1#编号不正确"                   dc:"编号"`{{range $cname, $c := .data.Fields }}{{if eq $c.Name "id" "created_at" "updated_at" "deleted_at"}}{{else}}
+	{{$c.Name | ToCamel }} {{$c.GType}} `json:{{$c.JsonTag | ToPadAndQuote}}                               dc:"{{$c.Comment}}"`{{end}}{{end}}
+}
+type {{ .data.TableName | ToCamel }}UpdateRes struct {
+}
+
+// 删除
+type {{ .data.TableName | ToCamel }}DeleteReq struct {
+	g.Meta `path:"/{{ .data.TableName | ToSnake }}/delete/{id}" method:"post" tags:"用户管理" summary:"删除指定用户"`
 	Id     uint `json:"id" in:"path"        dc:"用户编号"`
 }
-type UserDeleteRes struct {
-}
-type UserPermissionsReq struct {
-	g.Meta      `path:"/user/permission/{id}" method:"post" tags:"用户管理" summary:"设置用户权限"`
-	Id          uint   `json:"id" in:"path"        v:"min:1#用户的编号不正确"  dc:"用户编号"`
-	Permissions string `json:"permissions"         v:"required|length:3,500" dc:"用户权限"`
-}
-type UserPermissionsRes struct {
-}
-type UserLoginReq struct {
-	g.Meta   `path:"/login" method:"post" tags:"用户状态" summary:"登录"`
-	UserName string `json:"username"        v:"required|min-length:4"    dc:"用户名"`  //
-	Password string `json:"password"        v:"required|ci"              dc:"用户密码"` //
-}
-type UserLoginRes struct {
-	User *model.UserLoginResponse `json:"user" dc:"用户信息"`
-}
-
-type UserLogoutReq struct {
-	g.Meta `path:"/logout" method:"post" tags:"用户状态" summary:"登出"`
-}
-type UserLogoutRes struct {
-}
-
-type PermissionsAllReq struct {
-	g.Meta `path:"/permissions/all" method:"get" tags:"权限记录" summary:"查询所有权限设置项"`
-}
-type PermissionsAllRes struct {
+type {{ .data.TableName | ToCamel }}DeleteRes struct {
 }
--- vendor/github.com/gogf/gf/cmd/gf/v2/internal/cmd/genapi/genapi_api.go
+++ vendor/github.com/gogf/gf/cmd/gf/v2/internal/cmd/genapi/genapi_api.go
@@ -8,95 +8,130 @@ package genapi
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/os/gview"
 	"github.com/gogf/gf/v2/text/gstr"
 
-	"github.com/gogf/gf/cmd/gf/v2/internal/consts"
 	"github.com/gogf/gf/cmd/gf/v2/internal/utility/mlog"
+	"github.com/gogf/gf/cmd/gf/v2/internal/utility/utils"
 )
 
-func generateApi(ctx context.Context, in CGenDaoInternalInput) {
+// 需要的数据:
+// - 项目对象
+// -   项目名
+// - 表对象
+// -   首字母大写表名
+// -   小写表名
+// -   表备注
+// - 字段对象
+// -   首字母大写字段名
+// -   字段 golang 类型
+// -   字段 json 名(小写字段名)
+// -   字段备注
+type ApiData struct {
+	Proj         string // 项目名，英文 import 使用路径
+	TableName    string // 表名，引文 首字母大写
+	NewTableName string
+	TableComment string               // 表备注
+	Fields       map[string]*ApiField // 字段对象
+}
+type ApiField struct {
+	gdb.TableField
+	GType   string
+	JsonTag string
+}
 
-	var dirPathEntity = gfile.Join(in.Path, in.ApiPath)
-	// Model content.
+func generateApi(ctx context.Context, in CGenDaoInternalInput) {
 	for i, tableName := range in.TableNames {
+		newTableName := in.NewTableNames[i]
+		data := ApiData{
+			Proj:         "",
+			TableName:    tableName,
+			NewTableName: newTableName,
+			TableComment: "", // in.DB.TableComment(ctx, tableName),
+		}
 		fieldMap, err := in.DB.TableFields(ctx, tableName)
 		if err != nil {
 			mlog.Fatalf("fetching tables fields failed for table '%s':\n%v", tableName, err)
 		}
 
-		var (
-			newTableName                    = in.NewTableNames[i]
-			entityFilePath                  = filepath.FromSlash(gfile.Join(dirPathEntity, gstr.CaseSnake(newTableName)+".go"))
-			structDefinition, appendImports = generateStructDefinition(ctx, generateStructDefinitionInput{
-				CGenDaoInternalInput: in,
-				TableName:            tableName,
-				StructName:           gstr.CaseCamel(newTableName),
-				FieldMap:             fieldMap,
-				IsDo:                 false,
-			})
-			_ = generateApiContent( // entityContent
-				ctx,
-				in,
-				newTableName,
-				gstr.CaseCamel(newTableName),
-				structDefinition,
-				appendImports,
-			)
-		)
-		in.generatedFilePaths.EntityFilePaths = append(
-			in.generatedFilePaths.EntityFilePaths,
-			entityFilePath,
-		)
-		t := NewTpl()
-		if err := t.Write(entityFilePath, "gen_templates/api.go.tpl", g.Map{
-			"in":        in,
-			"TableName": newTableName,
-			// "tableNameCamelCase": gstr.CaseCamel(newTableName),
-			// "structDefinition":   structDefinition,
-			// "imports":            appendImports,
+		data.Fields = getApiFields(ctx, fieldMap, generateStructDefinitionInput{
+			CGenDaoInternalInput: in,
+			TableName:            tableName,
+			StructName:           gstr.CaseCamel(newTableName),
+			FieldMap:             fieldMap,
+			IsDo:                 false,
+		})
+		entityFilePath := filepath.FromSlash(gfile.Join(in.Path, in.ApiPath, gstr.CaseSnake(newTableName)+".go"))
+		tpl := NewTpl()
+		tpl.Gv.BindFuncMap(gview.FuncMap{
+			"ToSnake": gstr.CaseSnake,
+			"ToCamel": gstr.CaseCamel,
+			"ToPadAndQuote": strToEqLen,
+		})
+		if err := tpl.Write(entityFilePath, "gen_templates/api.go.tpl", g.Map{
+			"data": data,
 		}); err != nil {
-			mlog.Fatal(ctx, err)
+			mlog.Fatalf("writing content to '%s' failed: %v", entityFilePath, err)
+		} else {
+			mlog.Print(ctx, "--------------------------------")
+			utils.GoFmt(entityFilePath)
+			mlog.Printf("generated: %s", entityFilePath)
 		}
-		// 以下模板测试通过
-		// {{ .in.Link }}
-		// {{range $key, $value := .in.TypeMapping}}
-		// - Mapping: {{$key}}, Type: {{$value.Type}}, Import: {{$value.Import}}
-		// {{end}}
-		// {range $filePath := .in.GeneratedFilePaths.EntityFilePaths}
-		// - Generated File Path: {$filePath}
-		// {end}
-		// {{range $tableName := .in.TableNames}}
-		// - Table Name: {{$tableName}}
-		// {{end}}
-		mlog.Print(ctx, "--------------------------------")
-		mlog.Printf("generated: %s", entityFilePath)
-
-		// err = gfile.PutContents(entityFilePath, strings.TrimSpace(entityContent))
-		// if err != nil {
-		// 	mlog.Fatalf("writing content to '%s' failed: %v", entityFilePath, err)
-		// } else {
-		// 	utils.GoFmt(entityFilePath)
-		// 	mlog.Print("generated:", entityFilePath)
-		// }
 	}
 }
 
-func generateApiContent(
-	ctx context.Context, in CGenDaoInternalInput, tableName, tableNameCamelCase, structDefine string, appendImports []string,
-) string {
-	entityContent := gstr.ReplaceByMap(
-		getTemplateFromPathOrDefault(in.TplDaoEntityPath, consts.TemplateGenDaoEntityContent),
-		g.MapStrStr{
-			tplVarTableName:          tableName,
-			tplVarPackageImports:     getImportPartContent(ctx, structDefine, false, appendImports),
-			tplVarTableNameCamelCase: tableNameCamelCase,
-			tplVarStructDefine:       structDefine,
-		},
-	)
-	entityContent = replaceDefaultVar(in, entityContent)
-	return entityContent
+// 保持返回的字符串长度始终是 20，如果长度不够，就在后面加 空格
+func strToEqLen(s string) string {
+	return fmt.Sprintf("\"%s\"%s", s, gstr.Repeat(" ", 20-len(s)))
+}
+
+func getApiFields(ctx context.Context, gField map[string]*gdb.TableField, in generateStructDefinitionInput) (apiFields map[string]*ApiField) {
+	apiFields = make(map[string]*ApiField)
+	for k, v := range gField {
+		apiFields[k] = &ApiField{
+			TableField: *v,
+			GType:      getGTypeByFType(ctx, v.Type, in),
+			JsonTag:    gstr.CaseSnakeFirstUpper(v.Name),
+		}
+	}
+	return
+}
+
+func getGTypeByFType(ctx context.Context, ltype string, in generateStructDefinitionInput) (gtype string) {
+	localTypeName, err := in.DB.CheckLocalTypeForField(ctx, ltype, nil)
+	if err != nil {
+		panic(err)
+	}
+	switch localTypeName {
+	case gdb.LocalTypeDate, gdb.LocalTypeDatetime:
+		if in.StdTime {
+			gtype = "time.Time"
+		} else {
+			gtype = "*gtime.Time"
+		}
+
+	case gdb.LocalTypeInt64Bytes:
+		gtype = "int64"
+
+	case gdb.LocalTypeUint64Bytes:
+		gtype = "uint64"
+
+	// Special type handle.
+	case gdb.LocalTypeJson, gdb.LocalTypeJsonb:
+		if in.GJsonSupport {
+			gtype = "*gjson.Json"
+		} else {
+			gtype = "string"
+		}
+	}
+	if gtype == "" {
+		gtype = "string"
+	}
+	return
 }
